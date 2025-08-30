d2a35e7c40bc14137bd1ac52fbc3d9b7a2336170
Carbon14-48 <mohammedberrichi4@gmail.com>
2025-08-30 21:20:56 +0100

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,46 @@
 import { useTheme } from "../customHooks/useTheme";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ShoppingCartCheckoutOutlinedIcon from "@mui/icons-material/ShoppingCartCheckoutOutlined";
+import logo from "../assets/logo.svg";
+
 export default function Navbar() {
   const { theme, toggleTheme } = useTheme();
   console.log(theme);
+
   return (
-    <nav className="bg-white dark:bg-black text-black dark:text-white  flex justify-between">
-      <p>Logo</p>
-      <ul className="flex gap-4">
+    <nav className="bg-slate-300 dark:bg-black text-black dark:text-white flex justify-between items-center px-6 py-3 shadow-md">
+      <img src={logo} alt="Logo" className="w-32 cursor-pointer" />
+
+      <ul className="flex gap-6 items-center">
         <li>
-          <button onClick={toggleTheme} className="">
+          <button
+            onClick={toggleTheme}
+            className="hover:bg-blue-500 dark:hover:bg-blue-500 rounded-full p-2 transition pb-3.5"
+          >
             {theme === "light" ? (
               <DarkModeOutlinedIcon />
             ) : (
-              <LightModeOutlinedIcon className="w-4 h-4" />
+              <LightModeOutlinedIcon />
             )}
           </button>
         </li>
-        <li>Home</li>
-        <li>Products</li>
-        <li>Profile</li>
+
+        <li className="cursor-pointer border-b-2 border-transparent  hover:border-blue-500 transition">
+          Home
+        </li>
+        <li className="cursor-pointer border-b-2 border-transparent hover:border-blue-500 transition">
+          Products
+        </li>
+        <li className="cursor-pointer flex gap-2 border-b-2 border-transparent p-1 hover:border-blue-500 transition">
+          <ShoppingCartCheckoutOutlinedIcon className="text-blue-500" />
+          Checkout
+        </li>
+        <li className="cursor-pointer flex gap-2 border-b-2 border-transparent p-1 hover:border-blue-500 transition">
+          <AccountCircleOutlinedIcon className="text-blue-500" />
+          Profile
+        </li>
       </ul>
     </nav>
   );
