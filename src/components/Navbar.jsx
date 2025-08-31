9d4c093d550b3c4cca921edaeaf1761008347435
Carbon14-48 <mohammedberrichi4@gmail.com>
2025-08-31 02:28:36 +0100

    responsiviy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,11 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 import ShoppingCartCheckoutOutlinedIcon from "@mui/icons-material/ShoppingCartCheckoutOutlined";
 import logo from "../assets/logo.svg";
 import { Link } from "react-router-dom";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import { useState } from "react";
 export default function Navbar() {
   const { theme, toggleTheme } = useTheme();
+  const [isCollapsed, setIsCollapsed] = useState(true);
   console.log(theme);
 
   return (
@@ -26,21 +29,38 @@ export default function Navbar() {
             )}
           </button>
         </li>
-
-        <li className="cursor-pointer border-b-2 border-transparent hover:border-blue-500 transition">
-          <Link to="/">Home</Link>
+        <li>
+          <button
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            className="cursor-pointer hover:bg-blue-500/20 dark:hover:bg-blue-500/20 rounded-full transition pb-2"
+          >
+            <MenuOutlinedIcon className="sm:sr-only" />
+          </button>
         </li>
-        <li className="cursor-pointer border-b-2 border-transparent hover:border-blue-500 transition">
-          <Link to="/products">Products</Link>
-        </li>
-        <li className="cursor-pointer flex gap-2 border-b-2 border-transparent p-1 hover:border-blue-500 transition">
-          <ShoppingCartCheckoutOutlinedIcon className="text-blue-500" />
-          <Link to="/checkout">Checkout</Link>
-        </li>
-        <li className="cursor-pointer flex gap-2 border-b-2 border-transparent p-1 hover:border-blue-500 transition">
-          <AccountCircleOutlinedIcon className="text-blue-500" />
-          <Link to="/user">Profile</Link>
-        </li>
+        <div
+          className={`
+  flex gap-6 items-center transition-all duration-300
+  max-sm:absolute max-sm:top-full max-sm:left-0 max-sm:w-full 
+  max-sm:bg-white/90 max-sm:dark:bg-black/90 max-sm:backdrop-blur-md
+  max-sm:flex-col max-sm:py-4
+  ${isCollapsed ? "max-sm:hidden" : "max-sm:flex"}
+`}
+        >
+          <li className="cursor-pointer border-b-2 border-transparent hover:border-blue-500 transition">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="cursor-pointer border-b-2 border-transparent hover:border-blue-500 transition">
+            <Link to="/products">Products</Link>
+          </li>
+          <li className="cursor-pointer flex gap-2 border-b-2 border-transparent p-1 hover:border-blue-500 transition">
+            <ShoppingCartCheckoutOutlinedIcon className="text-blue-500" />
+            <Link to="/checkout">Checkout</Link>
+          </li>
+          <li className="cursor-pointer flex gap-2 border-b-2 border-transparent p-1 hover:border-blue-500 transition">
+            <AccountCircleOutlinedIcon className="text-blue-500" />
+            <Link to="/user">Profile</Link>
+          </li>
+        </div>
       </ul>
     </nav>
   );
